dc58fd860b871a93dc57597414acbdfa413a9e7a
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-17 20:56:05 -0300

    Cambios en matrices

--- Matrices/Program.cs
+++ Matrices/Program.cs
@@ -19,4 +19,34 @@ foreach (int i in array2)
 {
     Console.Write(i + " ");
 }
-Console.Write(")");
+Console.Write(")\n");
+
+//Metodos
+Console.WriteLine($"El promedio del array 2 es: {array2.Average()}");
+
+
+//Array 3
+
+int[] array3 = { 1, 2, 3,4,5,6};
+Console.Write("Array 3 = (");
+foreach (int i in array3)
+{
+    Console.Write(i + " ");
+}
+Console.Write(")\n\n");
+
+//2D
+//Declaramos un array conociendo el numero de elementos pero sin los elementos
+
+Console.WriteLine("Array de 2D");
+int[,] arr2D = new int[2,2];
+for (int i = 0; i< 2; i++) {
+    for (int j = 0; j <2; j++) {
+        arr2D[i,j] = (i +j);
+        Console.Write(arr2D[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+ 
+
